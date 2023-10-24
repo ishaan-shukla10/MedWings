767090da39dc8f9ae707e0d6149045723c60051f
ishaan-shukla10 <106727665+ishaan-shukla10@users.noreply.github.com>
2023-10-24 16:54:40 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,22 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\ishaa\OneDrive\Desktop\OCR\Tesseract-OCR\tesseract"
+
+
+images_array = []
+
+
+num_images = int(input("Enter number of images: "))
+for i in range (num_images):
+    images_array.append(str(input("Enter image full path: ")))
+
+
+for i in range (len(images_array)):
+    img = cv2.imread(images_array[i])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    dta = pytesseract.image_to_string(img)
+    dta_array = dta.split('\n')
+    for j in range (len(dta_array)):
+        #print("Line ", j+1)
+        print(dta_array[j])
