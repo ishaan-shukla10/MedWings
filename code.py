4b209e78f02a1a3acf30a039b2fbda3716940300
ishaan-shukla10 <106727665+ishaan-shukla10@users.noreply.github.com>
2023-10-25 15:34:41 +0530

    Update code.py

--- code.py
+++ code.py
@@ -15,7 +15,7 @@ for i in range (num_images):
 for i in range (len(images_array)):
     img = cv2.imread(images_array[i])
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    dta = pytesseract.image_to_string(img)
+    dta = pytesseract.image_to_string(img, config = "--psm 6 --oem 3 -c tessedit_char_unblacklist=0123456789")
     dta_array = dta.split('\n')
     for j in range (len(dta_array)):
         #print("Line ", j+1)
